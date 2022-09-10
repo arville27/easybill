8b8089d202592023360d04c727479ac5e2bdd3d2
Daniel Wijaya <arville27@users.noreply.github.com>
2022-09-10 16:44:13 +0700

    Initial Commit

---
+++ src/main/java/net/arville/easybill/EasybillApplication.java
@@ -0,0 +1,13 @@
+package net.arville.easybill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasybillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/arville/easybill/EasybillApplicationTests.java
@@ -0,0 +1,13 @@
+package net.arville.easybill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
