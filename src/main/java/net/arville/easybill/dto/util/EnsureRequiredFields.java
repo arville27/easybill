8e677539e2c516e924adbc734e32a49abaa0bdcd
Daniel Wijaya <arville27@users.noreply.github.com>
2022-10-06 23:13:10 +0700

    Hide missing property from response and swagger ui

--- src/main/java/net/arville/easybill/dto/util/EnsureRequiredFields.java
+++ src/main/java/net/arville/easybill/dto/util/EnsureRequiredFields.java
@@ -1,8 +1,11 @@
 package net.arville.easybill.dto.util;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.Set;
 
 public interface EnsureRequiredFields {
 
+    @JsonIgnore
     Set<String> getMissingProperties();
 }
