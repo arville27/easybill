a78c529d86271eab84b4727328f6cafedbbbbc6c
Daniel Wijaya <arville27@users.noreply.github.com>
2022-10-07 18:59:11 +0700

    Revert "Fix miss calculation bug"
    This reverts commit 20e5e5efb6c4dc1009e235a55f0a95f1e28d4cc2.

--- src/main/java/net/arville/easybill/service/implementation/BillManagerImpl.java
+++ src/main/java/net/arville/easybill/service/implementation/BillManagerImpl.java
@@ -50,7 +50,7 @@ public class BillManagerImpl implements BillManager {
         List<OrderDetail> orderList = orderHeader.getOrderDetailList();
 
         BigDecimal totalOrderAmount = orderList.stream()
-                .map(order -> BigDecimal.valueOf(order.getQty()).multiply(order.getPrice()))
+                .map(OrderDetail::getPrice)
                 .reduce(BigDecimal.valueOf(0), BigDecimal::add);
 
         BigDecimal discountAmountBeforeUpto = totalOrderAmount.multiply(BigDecimal.valueOf(discount));
