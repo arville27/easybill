e90f3debb27329bde845da8b32908b0bc0250cef
Daniel Wijaya <arville27@users.noreply.github.com>
2022-11-19 09:29:43 +0700

    Update database seeder

--- src/main/java/net/arville/easybill/configuration/RestSecurityConfig.java
+++ src/main/java/net/arville/easybill/configuration/RestSecurityConfig.java
@@ -98,7 +98,7 @@ public class RestSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(12);
     }
 
     @Bean
