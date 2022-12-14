0c48f4af9cffc781f7f11a3e7f6203f35889dbd1
Daniel Wijaya <arville27@users.noreply.github.com>
2022-12-14 21:42:30 +0700

    Adjust application constant - Reduce JWT valid period - Remove debugging origins

--- src/main/java/net/arville/easybill/constant/EasybillConstants.java
+++ src/main/java/net/arville/easybill/constant/EasybillConstants.java
@@ -6,13 +6,10 @@ import java.util.List;
 public final class EasybillConstants {
     public static final String TOKEN_SECRET = "easybill.token.secret";
     public static final String AUTH_PATH = "/api/auth";
-    public static final Date JWT_EXPIRY_TIME = new Date(System.currentTimeMillis() + 30L * 24 * 60 * 60 * 1000); // 30 Days
+    public static final Date JWT_EXPIRY_TIME = new Date(System.currentTimeMillis() + 3 * 24 * 60 * 60 * 1000); // 3 Days
     public static final List<String> ALLOWED_ORIGINS = List.of(
             "http://localhost:4200",
-            "http://10.20.153.199:4200",
-            "http://192.168.100.96:4200",
             "https://easybill.arville.net",
-            "http://192.168.43.33:4200",
             "https://easybill-beta.arville.net",
             "http://10.10.10.2:4200"
     );
